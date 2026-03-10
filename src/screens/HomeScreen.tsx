b2352cd98f93aa031639b2e31cbe13235d8f4d7f
PTMaster Dev <ptmaster@dev.local>
2026-03-10 13:40:35 +0900

    docs: 홈 화면 명언 문구 수정

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -25,10 +25,10 @@ import { useTheme } from '../context/ThemeContext';
 import { useWorkout, MINI_BAR_HEIGHT } from '../context/WorkoutContext';
 
 const GREETINGS = [
-  '운동의 절반은 현관문을 열고 나가는 것',
-  '운동 안하면 빨리 늙는다',
-  '오늘 운동 많이 된다. 잠 잘올꺼야',
-  '운동은 하루를 짧게 하지만, 인생을 길게 만든다.',
+  '"현관문을 열고 나가면 이미 운동의 시작"',
+  '"운동 안하면 빨리 늙는다"',
+  '"운동 많이 된다. 오늘 밤에 푹 잘꺼야"',
+  '"운동은 하루를 짧게 하지만, 인생을 길게 만든다"',
 ];
 
 type Nav = NativeStackNavigationProp<RootStackParamList>;
